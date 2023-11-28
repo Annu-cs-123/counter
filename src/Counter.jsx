26cf6ff08e2e06db632100c95d7241c7bdbed0c6
annu-cs-123 <annukjind15@gmail.com>
2023-11-28 11:14:20 +0530

    bg changer

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -18,12 +18,15 @@ export default function Counter() {
 
   return (
     <div>
-      <p>
+      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+      <p className="text-3xl font-bold  text-center my-2 py-2">
         {counter}
       </p>
-      <button onClick={SETCounterValue}>add value +</button>
+      <button onClick={SETCounterValue} className="outline-none px-4 py-3 mx-1 rounded-full text-white shadow-lg"
+        style={{ backgroundColor: "Green" }}> add value +</button>
       {/* <button onClick={() => setCounter((counter) => counter - 1)}>Remove value -</button> */}
-      <button onClick={setRemoveValue}>Remove value -</button>
+      <button onClick={setRemoveValue} className="outline-none px-4 mx-1 py-3 rounded-full text-white shadow-lg"
+        style={{ backgroundColor: "red" }}>Remove value -</button>
     </div >
   )
 
